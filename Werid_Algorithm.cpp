1b28a7713ab1bc14b881aae5b0014dfd44a99795
aman-panwar <aman.panwar2002@gmail.com>
2022-12-13 23:44:22 +0530

    added some solutions

--- Werid_Algorithm.cpp
+++ Werid_Algorithm.cpp
@@ -1,17 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define ull unsigned ll
+#define rint(x) int x; cin>>x;
+#define rll(x) ll x;cin>>x;
+#define vi vector<int>
+#define vll vector<ll>
+#define rvi(x,y) vi x(y); for(auto &i:x)cin>>i;
+#define rvll(x,y) vll x(y); for(auto &i:x)cin>>i;
+#define tc rll(__TEST_CASE__);for(int __test_case__ =1; __test_case__<=__TEST_CASE__;__test_case__++)
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    unsigned long long n=3;
-    cin>>n;
+    FAST_IO
+    rll(n);
     while(n!=1)
     {
         cout<<n<<" ";
-        if(n%2==0)n/=2;
-        else n = n*3+1;
+        n = n%2?n*3+1:n/2;
     }
     cout<<"1\n";
 }
